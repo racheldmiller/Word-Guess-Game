2f4dc2f6700569dcb545b43db93cda2e454b665c
racheldmiller <46205109+racheldmiller@users.noreply.github.com>
2019-03-30 07:39:16 -0700

    Tried to change the JS, but the code still isn't working...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,55 +10,64 @@ var maxGuesses = 10; // how many guesses the player has
 var guessesRemaining = 0; // how many guesses are remaining 
 
 // players guesses 
+var blanksLetters = []; // both blank and solved letters
 var guessedLetters = []; // stores guessed letters
 var wrongGuesses = []; // stores wrong guesses
 var ansWordArr = [];
-var ansWord; 
+var ansWord = "";
 
 // the token booleans
-ifFinished = true; 
-letterInWord= true;
+isFinished = true; 
+letterInWord = true;
 
 // start game 
 function setup() {
 
+    // start game
+    maxGuesses = 10;
+
     // word is randomly chosen from the list
     ansWord = words[Math.floor(Math.random() * words.length)];
 
-    ansWordArr = [];
+    // split word into individual letters
+    ansWordArr = ansWord.split("");
 
-    // adds "_" to ansWordArr ... here's my for loop
-    for (var i = 0; i < ansWord.length; i++) { 
-        ansWordArr[i] = "_";
+    // count number of letters in word
+    blanks = ansWordArr.length;
+
+    // adds "_" to blanks ... here's my for loop
+    for (var i = 0; i < blanks; i++) { 
+        blanksLetters.push("_");
     }
 
     // resetting after rounds
-    numGuessesRemaining = maxNumGuesses; 
+    blanksLetters = [];
+    gessesRemaining = maxGuesses; 
     guessedLetters = [];
     wrongGuesses = []; 
 
-    // display gifs of shows 
-    document.getElementById("giphy-embed").src = "";
+    // testing
+    console.log(ansWord); // to print word in console 
+    console.log(blanksLetters); // to print blanks in console
 
     // to warn the player of running out of guesses
     document.getElementById("numGuesses").style.color = "";
 
     //show the selected elements on the screen 
     updateScreen();
-    
-};
 
-// testing
-console.log(ansWord); // to print word in console 
+    // updating HTML 
+    function updateScreen() {
+        document.getElementById("wins").innerText = Wins; // prints wins, restarts game
+        document.getElementById("losses").innerText = Losses; // prints losses, restarts game
+        document.getElementById("guesses").innerText = guessesRemaining; // prints guesses left
+        document.getElementById("ansWord").innerText = ansWordArr.join(""); // prints blanks and guesses 
+        document.getElementById("wrongGuesses").innerText = wrongGuesses.join(""); // prints incorrect letters
+        document.getElementById("guessedLetters").innerText = guessedLetters; // prints guessed letters
+    };
 
-// updating HTML 
-function updateScreen() {
-    document.getElementById("wins").innerText = Wins; // prints wins, restarts game
-    document.getElementById("losses").innerText = Losses; // prints losses, restarts game
-    document.getElementById("guesses").innerText = guessesRemaining; // prints guesses left
-    document.getElementById("ansWord").innerText = ansWordArr.join(""); // prints blanks and guesses 
-    document.getElementById("wrongGuesses").innerText = wrongGuesses.join(""); // prints incorrect letters
-    document.getElementById("guessedLetters").innerText = guessedLetters; // prints guessed letters
+    // display gifs of shows 
+    document.getElementById("giphy-embed").src = "";
 };
 
 // check for winners 
@@ -91,55 +100,46 @@ function winner() {
     };
 };
 
+// key activity 
+function checkGuess(letter) {
+    
+    // current state
+    var letterInWord = false;
+    
+    // If letter is in the word
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+            if (ansWord[i] === letter) {
+                blanksLetters[i] = letter;
+            }
+        }
+    }
+
+    else {
+        wrongGuesses.push(letter);
+        maxGuesses--; 
+    };
+}; 
+
 // check for losers 
 function loser() {
-    // if numGuessesRemaining = 0, add +1 to losses
-    if (guessCount === 0) {
+    // if guessesRemaining = 0, add +1 to losses
+    if (maxGuesses === 0) {
         losses++;
         alert("As If!");
         isFinished = true;
 
         //play the loser gif
         document.getElementById("giphy-embed").src = "https://giphy.com/gifs/3og0IEeKFFlzaykixW/html5";
-        document.getElementById("numLosses").style.color = "#FF0000";
+        document.getElementById("losses").style.color = "#FF0000";
     }
 };
-
-// key activity 
-function checkGuess(letter) {
-
-    // if a letter isn't in the guessedLetters arr, push the letter to the array
-    if (guessedLetters.indexOf(letter) === -1) {
-        guessedLetters.push(letter);
-
-        // if the letter isn't in the random word, numGuessesRemaining -1
-        if (ansWord.indexOf(letter) === -1) {
-            numGuessesRemaining--;
-
-        // if numGuessesRemaining is < 3 or less, change color
-            if (numGuessesRemaining <=3) {
-                document.getElementById("numGuesses").style.color = "#FF0000";
-            }
-        } 
-        // if letter is in word, replace "_" with the letter
-        else { 
-            for (var i = 0; i < ansWord.length; i++) {
-                if (ansWord[i] === letter) {
-                    ansWordArr[i] = letter;
-                };
-            };                
-        };
-    };
-}; 
 
 // other important functionality for keys
 document.onkeyup = function(event) { // captures key clicks
     // Converts all key clicks to uppercase letters... because that's more FUN.
-      var letterGuessed = String.fromCharCode(event.which).toUpperCase();
+    var guessedLetters = String.fromCharCode(event.which).toUpperCase();
 
     // Runs the code to check for correctness.
-      checkGuess(letter);
-
-    // Runs the code after each round is done.
-      updateScreen();
+    checkGuess(letter);
 };
